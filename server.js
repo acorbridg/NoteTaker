4d8623652f78e17f27c6e0bd47a0359772464f7d
ACorbridg <adybass@gmail.com>
2022-10-14 20:22:10 -0600

    added routes to server.js, get and post functions

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs')
+const { v4: uuidv4 } = require('uuid');
 const noteData = require('./db/db.json');
 const PORT = 3001;
 
@@ -8,18 +9,22 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.use(express.static('public', {extensions: ["html"]}));
 
 app.get('/api/notes', (req, res) => res.json(noteData));
     //GET /api/notes read the db.json file return all saved notes as JSON
 
 app.post('/api/notes', (req, res) => {
-    //add .post stuff to receive a new note to save on the request body, add it to the db.json file, then return the new note. 
+  const newNote = {
+    id: uuidv4(),
+    title: req.body.title,
+    text: req.body.text,
+  }  
+  noteData.push(newNote)
+  fs.writeFileSync('./db/db.json', JSON.stringify(noteData))
+  res.json(newNote)
 
+    //add .post stuff to receive a new note to save on the request body, add it to the db.json file, then return the new note. 
 })
 
 app.listen(PORT, () => {
